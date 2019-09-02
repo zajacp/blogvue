f61fd1e3f3b0dbf37d9f1865a1cb442209ed005a
Przemysław Zając <zajac@netkodo.com>
2019-09-02 22:51:16 +0200

    First configuration vue rails

---
+++ app/javascript/app.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <div id="app">
+    <NavigationTop></NavigationTop>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+
+  import NavigationTop from "./page_main_parts/navigation_top";
+  export default {
+    components: {NavigationTop},
+    data: function () {
+      return {
+        message: "Hello Vue!"
+      }
+    }
+  }
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
---
+++ app/javascript/page_main_parts/navigation_top.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="nav">
+    <span v-for="link in navigationLinks">
+      {{link}}
+    </span>
+  </div>
+</template>
+
+<script>
+	export default {
+		data: function () {
+			return {
+		    navigationLinks: [
+		    	'nav1',
+          'nav2',
+          'nav3'
+        ]
+			}
+		}
+	}
+</script>
+
+<style scoped>
+  span {
+    font-size: 2em;
+    padding-left: 5px;
+    padding-right: 5px;
+  }
+</style>
